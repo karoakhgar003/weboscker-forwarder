2115b65a0b762339f574cd54f1c381ea273d31c0
Karo Akhgar <95443191+karoakhgar003@users.noreply.github.com>
2026-06-15 02:15:06 +0330

    karo

---
+++ main.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const httpProxy = require('http-proxy');
+
+const proxy = httpProxy.createProxyServer({
+  target: 'ws://bb.7ir.me:49647',
+  ws: true,
+  changeOrigin: true
+});
+
+const server = http.createServer((req, res) => {
+  proxy.web(req, res, { target: 'http://bb.7ir.me:49647' });
+});
+
+// Handle WebSocket upgrade requests
+server.on('upgrade', (req, socket, head) => {
+  proxy.ws(req, socket, head);
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Proxy running on port ${PORT}`));
